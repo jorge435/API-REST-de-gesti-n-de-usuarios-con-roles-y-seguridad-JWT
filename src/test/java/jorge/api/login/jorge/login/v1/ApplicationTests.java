27bdc99e4b5486dab6478d064320bfa8604981dc
Jorge <jrge435@gmail.com>
2025-06-14 16:01:14 -0500

    commit inicial project

---
+++ src/test/java/jorge/api/login/jorge/login/v1/ApplicationTests.java
@@ -0,0 +1,13 @@
+package jorge.api.login.jorge.login.v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
