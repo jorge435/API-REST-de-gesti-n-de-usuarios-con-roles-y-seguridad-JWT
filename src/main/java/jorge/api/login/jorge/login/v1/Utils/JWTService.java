bbc581942774adf3b4b7dc4679b00cdf4cea6349
Jorge <jrge435@gmail.com>
2025-06-16 17:07:38 -0500

    feat: utils service

--- src/main/java/jorge/api/login/jorge/login/v1/Utils/JWTService.java
+++ src/main/java/jorge/api/login/jorge/login/v1/Utils/JWTService.java
@@ -1,4 +1,9 @@
 package jorge.api.login.jorge.login.v1.Utils;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class JWTService {
+    private static final String SECRET_KEY = "5A7234753778214125442A462D4A614E645267556B58703273357638792F423F";
+
 }
